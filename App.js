fd937b248e705d4005305c89fcfe2f059b0e9803
Sebastianamayag <44688416+Sebastianamayag@users.noreply.github.com>
2021-09-13 15:50:24 -0500

    formulario

--- App.js
+++ App.js
@@ -1,14 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Formik, useFormikContext, useField } from 'formik';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import * as Yup from 'yup';
+
+const MyInput = ({ fieldName, ...props }) => {
+  const [field] = useField(fieldName);
+  return (
+    <TextInput style={styles.input} onChangeText={field.handleChange('email')} value={field.value} {...props} />
+  )
+}
+
+const EmailForm = () => {
+  const { submitForm } = useFormikContext();
+  return (
+    <>
+      <Text>Correo electronico</Text>
+      <MyInput fieldName="email" />
+      <MyInput fieldName="name" />
+      <Button onPress={submitForm} title="Enviar" />
+    </>
+  )
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.container} >
+      <Formik
+        onSubmit={x => console.log(x)}
+        validationSchema={Yup.object({
+          email: Yup.string()
+            .email('Correo invalido')
+            .required('Requerido'),
+          name: Yup.string()
+            .min(50)
+            .required('Requerido'),
+        })}
+        initialValues={{ email: '' }}
+      >
+        <EmailForm />
+      </Formik>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +50,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    height: 12,
+    width: 150,
+    backgroundColor: '#eee'
+  }
 });
